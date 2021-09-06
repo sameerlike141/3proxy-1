d9c7f374afe2599e1b6a817f16335cc43c6daa80
sameerlike141 <sameerlike141@gmail.com>
2021-09-06 19:12:00 +0500

    Update socks.sh

--- socks.sh
+++ socks.sh
@@ -1,7 +1,7 @@
 apt-get update && apt-get -y upgrade
 apt install megatools
 apt-get install -y build-essential nano
-megadl 'https://mega.nz/#!5VgBiCKA!NFp1cWKyF4M-8H87JbKburqwffgj4HDalN65kfrTx0E'
+megadl 'https://mega.nz/#!MVRCGJzA!NFp1cWKyF4M-8H87JbKburqwffgj4HDalN65kfrTx0E'
 tar xzf socks5.tar.gz
 rm socks5.tar.gz
 cd 3proxy-3proxy-0.8.6
